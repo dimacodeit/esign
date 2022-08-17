55fc20bc56b761a08f8e5cf7e7a9311a266f4b0b
dmitry <dmitryspiridonov56@gmail.com>
2022-08-17 17:30:09 +0300

    add eslint airbnb config

--- src/features/counter/counterAPI.ts
+++ src/features/counter/counterAPI.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-promise-executor-return */
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise<{ data: number }>((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+function fetchCount(amount = 1) {
+  return new Promise<{ data: number }>(
+    (resolve) => setTimeout(() => resolve({ data: amount }), 500),
   );
 }
+
+export default fetchCount;
